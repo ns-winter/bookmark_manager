f322c6e9ed938544008e65adca379995631be477
Nigel Said <nigelsaid@gmail.com>
2017-05-02 15:29:07 +0100

    adds new link feature

--- app/app.rb
+++ app/app.rb
@@ -4,7 +4,16 @@ require_relative './models/link'
 class BookmarkManager < Sinatra::Base
 
   get '/links' do
-  @links = Link.all
-  erb :'links/index'
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  post '/links' do
+    Link.create(title: params[:title], url: params[:url])
+    redirect '/links'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
   end
 end
---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,9 @@
+feature 'Adding links' do
+  scenario 'adds YouTube link' do
+    visit '/links/new'
+    fill_in 'title', with: 'YouTube'
+    fill_in 'url', with: 'www.youtube.com'
+    click_button 'Submit'
+    expect(page).to have_content 'YouTube'
+  end
+end
