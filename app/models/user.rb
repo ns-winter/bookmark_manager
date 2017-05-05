a0e7af6703e9a9aa9d2f85710afc261434302d79
Nigel Said <nigelsaid@gmail.com>
2017-05-05 18:41:29 +0100

    adds email entry validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User
   validates_confirmation_of :password
 
   property :id, Serial
-  property :email, String
+  property :email, String, format: :email_address, required: true
   property :password_digest, Text
 
   def password=(password)
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,12 +1,10 @@
 feature 'User sign up' do
+
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
-end
-
-feature 'User sign up' do
 
   scenario 'requires a matching confirmation password' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
@@ -14,4 +12,12 @@ feature 'User sign up' do
     expect(page).to have_content 'Password and confirmation password do not match'
   end
 
+  scenario "I can't sign up without an email address" do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+  end
+
+  scenario "I can't sign up with an invalid email address" do
+    expect { sign_up(email: "invalid@email") }.not_to change(User, :count)
+  end
+  
 end
